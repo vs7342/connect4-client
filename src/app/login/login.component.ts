d5310a0d3b9cda11312ee93f53c3170505ae3c32
Vidit Singhal <vs7342@g.rit.edu>
2017-09-27 00:17:51 -0400

    Added signup and login functionality in appropriate components.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild, ViewContainerRef} from '@angular/core';
+import {Router} from '@angular/router';
+import {ToastsManager} from 'ng2-toastr';
+import {UserService} from '../services/user.service';
+import {DataService} from "../services/data.service";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  // Access the form element
+  @ViewChild('loginForm') loginForm: HTMLFormElement;
 
-  constructor() { }
+  constructor(
+    private route: Router,
+    private toaster: ToastsManager,
+    vcr: ViewContainerRef,
+    private userService: UserService,
+    private dataService: DataService) {
+      this.toaster.setRootViewContainerRef(vcr);
+  }
 
   ngOnInit() {
+  }
+
+  // This will check for login credentials with the API
+  // Will navigate to lobby page if login is successful.. Else displays an error message.
+  onLoginClick() {
+    // Proceed only if form is valid
+    if (this.loginForm.valid) {
+      // Check for credentials here
+      this.userService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe(
+        (data => {
+          if (data.success) {
+            // Save user data
+            this.dataService.setCurrentUser(data.user);
+            // Navigate to Lobby
+            this.route.navigate(['/lobby']);
+          }else {
+            // Display authentication error
+            this.toaster.error('Invalid email / password', 'Authentication error');
+          }
+        }),
+        (error => {
+          // Error in response
+          this.toaster.error(error, 'Unable to login.');
+        })
+      );
+    }
   }
 
 }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild, ViewContainerRef} from '@angular/core';
+import {UserService} from '../services/user.service';
+import {ToastsManager} from 'ng2-toastr';
+import {Router} from '@angular/router';
+import {DataService} from "../services/data.service";
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +10,63 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  // Access the sign up form
+  @ViewChild('signupForm') signupForm: HTMLFormElement;
 
-  constructor() { }
+  constructor(
+    private userService: UserService,
+    private route: Router,
+    private toaster: ToastsManager,
+    vcr: ViewContainerRef,
+    private dataService: DataService
+  ) {
+      this.toaster.setRootViewContainerRef(vcr);
+  }
 
   ngOnInit() {
+  }
+
+  /**
+   * Sends a sign-up request to the API
+   * Will navigate to lobby page if sign up was successful.. Else displays an error message.
+   */
+  onSignupClick() {
+    // If form is invalid, do not proceed
+    if (this.signupForm.valid) {
+      const formValue = this.signupForm.value;
+      // Call the sign up method of user service
+      this.userService.signup(
+        formValue.email,
+        formValue.fname,
+        formValue.lname,
+        formValue.screenName,
+        formValue.password
+      ).subscribe(
+        // Response completed successfully
+        (data => {
+          if (data.success) {
+            // Save User data
+            this.dataService.setCurrentUser(data.user);
+            // Navigate to Lobby
+            this.route.navigate(['/lobby']);
+          } else {
+            // Display error message from server
+            this.toaster.error(data.message, 'Sign up error');
+          }
+        }),
+        // Response error
+        (error => {
+          this.toaster.error(error, 'Unable to sign up.');
+        })
+      );
+    }
+  }
+
+  /**
+   * Resets the sign up form
+   */
+  onResetClick() {
+    this.signupForm.reset();
   }
 
 }
